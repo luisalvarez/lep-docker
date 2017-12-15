b92e1be967db24fd12839a5170c4c88d887dad0b
Luis <l.alvarez@gbh.com.do>
2017-12-15 15:23:00 -0400

    change to old version

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# chown -R www-data:www-data /app
-# chmod -R 777 /app/storage /app/bootstrap/cache
-# php /app/artisan migrate
-chmod -R 777 /app
+chown -R www-data:www-data /app
+chmod -R 777 /app/storage /app/bootstrap/cache
+php /app/artisan migrate
 exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf
